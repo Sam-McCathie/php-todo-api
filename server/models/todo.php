074231ac13232d4f39e39dc0441439a8276d6414
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-04-09 10:32:32 +1200

    Re-order model methods to match controller

--- server/models/todo.php
+++ server/models/todo.php
@@ -6,6 +6,23 @@ class TodoModel {
         $this->pdo = $pdo;
     }
 
+    public function getTodo($userId, $todoId){
+        if($todoId){
+            $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE user_id = :userId AND todo_id = :todoId');
+            $stmt->execute(["userId"=>$userId, "todoId"=>$todoId]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE user_id = :userId');
+            $stmt->execute(["userId"=>$userId]);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        return [
+            "status" => "success",
+            "message" => "Todo(s) retrieved successfully.",
+            "data" => $result
+        ];
+    }
+    
     public function createTodo($userId, $text){
         $stmt = $this->pdo->prepare('INSERT INTO todos (user_id, text) VALUES (:userId, :text)');
         $stmt->execute(["userId"=> $userId, "text"=>$text]);
@@ -26,22 +43,6 @@ class TodoModel {
         ];
     }
 
-    public function getTodo($userId, $todoId){
-        if($todoId){
-            $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE user_id = :userId AND todo_id = :todoId');
-            $stmt->execute(["userId"=>$userId, "todoId"=>$todoId]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        } else {
-            $stmt = $this->pdo->prepare('SELECT * FROM todos WHERE user_id = :userId');
-            $stmt->execute(["userId"=>$userId]);
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-        return [
-            "status" => "success",
-            "message" => "Todo(s) retrieved successfully.",
-            "data" => $result
-        ];
-    }
 
     public function deleteTodo($todoId){
        $stmt = $this->pdo->prepare('DELETE FROM todos WHERE todo_id = :todoId');
